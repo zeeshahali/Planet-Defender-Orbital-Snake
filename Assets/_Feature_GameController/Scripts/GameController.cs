bc633a222ad70a986af4fdfd405daba0bec37ad1
Zeeshah Butt <z3eshahali@gmail.com>
2025-12-24 17:31:12 +0500

    Changed framerate to 120

--- Assets/_Feature_GameController/Scripts/GameController.cs
+++ Assets/_Feature_GameController/Scripts/GameController.cs
@@ -13,7 +13,7 @@ public class GameController : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 120;
     }
 
     private void Start()
